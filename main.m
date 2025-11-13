c215c497f1b5b770aac22b2b7da2f6ccf47d4fe5
FrancoAnco <francescobranca2000@gmail.com>
2025-11-13 19:54:30 +0100

    Update main.m

--- main.m
+++ main.m
@@ -1,10 +1,11 @@
 
 % Esegue il training
-[Q, initStates, episodesData] = trainTaxiQLearning(1000);
+[Q, initStates, episodesData] = trainTaxiQLearning(1000)
 
 % Visualizza l'episodio 1000
-plotEpisodeTaxi(Q, 1000, initStates, episodesData);
+plotEpisodeTaxi(Q, 1000, initStates, episodesData)
 
 % Curva di apprendimento
 
-plotTrainingRewards(episodesData);
+plotTrainingRewards(episodesData)
+
